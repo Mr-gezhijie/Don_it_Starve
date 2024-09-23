8713cd6985c349661dfbd9b5cb8952f188df3238
mr-gezhijie <1468531503@qq.com>
2024-09-24 01:45:25 +0800

    本地夜视Mod修理完成

--- src/main/AutoNightVision/modinfo.lua
+++ src/main/AutoNightVision/modinfo.lua
@@ -1,9 +1,41 @@
 local CHS = locale == "zh" or locale == "zhr"
 
-name = CHS and "本地夜视" or "Local Night Vision"
-description = CHS and "仅视觉效果，不能避免被查理攻击！" or "Visual effects only, can not avoid being attacked by Charlie!"
-author = "KyuubiRan"
-version = "1.0.5"
+name = "AutoNightVision(自动夜视)"
+description = CHS and [[
+仅视觉效果，不能避免被查理攻击！
+
+功能：
+1。黑夜的时候有夜视效果
+2。鼹鼠帽不再是黑白
+
+对原版的优化：
+1。加入 时间段 白天/黄昏/夜晚  提示效果
+2。聊天也能按键生效问题 以及不生效问题
+3。进游戏自动开启夜视
+4。加入语言设置，如果是不是中国的小伙伴，我不知道会不会生效
+
+一般功能都可以进行设置，如果还有那些地方不足欢迎留言
+
+此mod为搬运优化
+]] or [[
+Only visual effects cannot avoid being attacked by Charlie!
+
+Function:
+1. Night vision effect in the dark
+2. The mole hat is no longer black and white
+
+For the optimization of the original version:
+1. Add daytime/dusk/nighttime reminder effect
+2. The issue of button activation and deactivation during chat
+3. Automatically turn on night vision when entering the game
+4. Add language settings, I don't know if it will take effect if you are not a Chinese partner
+
+General functions can be set, if there are still areas that are insufficient, please feel free to leave a message
+
+This mod is for handling optimization
+]]
+author = "GEZHIJIE"
+version = "1.0.1"
 api_version = 10
 dst_compatible = true
 client_only_mod = true
@@ -12,7 +44,70 @@ icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 priority = -10000
 
+
+
+-- 处理绑定键
+local KEY_A = 65
+local keyslist = {}
+local string = ""
+for i = 1, 26 do
+    local ch = string.char(KEY_A + i - 1)
+    keyslist[i] = {description = ch, data = ch}
+end
+
 configuration_options = {
+    {
+        name = "FunctionSettings",
+        label = CHS and "功能设置" or "Function Settings",
+        options = { {
+                        description = "",
+                        data = ""
+                    } },
+        default = ""
+    },
+    {
+        name = "Language",
+        label = CHS and "语言" or "Language",
+        options = {
+            { description ="简体中文" , data = true },
+            { description = "English" , data = false }
+        },
+        default = CHS
+    },
+    {
+        name = "ToggleHotkey",
+        label = CHS and "快捷键" or "Shortcut Keys",
+        options = keyslist,
+        default = "N"
+    },
+    {
+        name = "EnterGameAutoOpen",
+        label = CHS and "进游戏自动开启" or "Enter Game Auto Open",
+        options = {
+            { description = CHS and "禁用" or "Disable", data = false },
+            { description = CHS and "启用" or "Enable", data = true }
+        },
+        default = true
+    },
+    {
+        name = "PresentStageWarn",
+        label = CHS and "时间阶段提醒" or "Time Stage Reminder",
+        options = {
+            { description = CHS and "禁用" or "Disable", data = false },
+            { description = CHS and "启用" or "Enable", data = true }
+        },
+        hover = CHS and "就是这一天的三个阶段，到了的时候进行提醒" or "There are three stages of this day, and reminders will be given when the time comes",
+        default = true
+    },
+    {
+        name = "AutoSettings",
+        label = CHS and "自动设置" or "Auto Settings",
+        options = { {
+                        description = "",
+                        data = ""
+                    } },
+        default = ""
+    },
     {
         name = "AutoEnableAtNight",
         label = CHS and "自动在夜晚时开启" or "Auto enable at night",
@@ -20,7 +115,7 @@ configuration_options = {
             { description = CHS and "禁用" or "Disable", data = false },
             { description = CHS and "启用" or "Enable", data = true }
         },
-        default = false
+        default = true
     },
     {
         name = "AutoEnableAtDusk",
@@ -38,39 +133,7 @@ configuration_options = {
             { description = CHS and "禁用" or "Disable", data = false },
             { description = CHS and "启用" or "Enable", data = true }
         },
-        default = false
+        default = true
     },
-    {
-        name = "ToggleHotkey",
-        label = CHS and "开启/关闭快捷键" or "Toggle hotkey",
-        options = {
-            { description = "A", data = 97 },
-            { description = "B", data = 98 },
-            { description = "C", data = 99 },
-            { description = "D", data = 100 },
-            { description = "E", data = 101 },
-            { description = "F", data = 102 },
-            { description = "G", data = 103 },
-            { description = "H", data = 104 },
-            { description = "I", data = 105 },
-            { description = "J", data = 106 },
-            { description = "K", data = 107 },
-            { description = "L", data = 108 },
-            { description = "M", data = 109 },
-            { description = "N", data = 110 },
-            { description = "O", data = 111 },
-            { description = "P", data = 112 },
-            { description = "Q", data = 113 },
-            { description = "R", data = 114 },
-            { description = "S", data = 115 },
-            { description = "T", data = 116 },
-            { description = "U", data = 117 },
-            { description = "V", data = 118 },
-            { description = "W", data = 119 },
-            { description = "X", data = 120 },
-            { description = "Y", data = 121 },
-            { description = "Z", data = 122 },
-        },
-        default = 110
-    }
+
 }
--- src/main/AutoNightVision/modmain.lua
+++ src/main/AutoNightVision/modmain.lua
@@ -1,7 +1,9 @@
 local AutoEnableAtNight = GetModConfigData("AutoEnableAtNight")
 local AutoEnableAtDusk = GetModConfigData("AutoEnableAtDusk")
 local AutoDisableAtDay = GetModConfigData("AutoDisableAtDay")
-local ToggleEnableKey = GetModConfigData("ToggleHotkey")
+local EnterGameAutoOpen = GetModConfigData("EnterGameAutoOpen")
+local PresentStageWarn = GetModConfigData("PresentStageWarn")
+local CHS = GetModConfigData("Language")
 
 local function setNightVisionEnabled(inst, bool)
     if bool == inst.local_nightvision then
@@ -14,7 +16,29 @@ local function setNightVisionEnabled(inst, bool)
     inst.components.playervision:SetCustomCCTable(bool and "images/colour_cubes/beaver_vision_cc.tex" or nil)
 end
 
+local function TIPS(title, content)
+    GLOBAL.ChatHistory:AddToHistory(GLOBAL.ChatTypes.Message, nil, nil,
+            title,
+            content, GLOBAL.PLAYERCOLOURS.CORAL)
+end
+
 local function autoEnable(inst)
+    if PresentStageWarn then
+        local title = CHS and "现时间阶段：" or "At this time stage:"
+        local c_night = CHS and "夜晚" or "NIGHT"
+        local c_dusk = CHS and "黄昏" or "DUSK"
+        local c_day = CHS and "白昼" or "DAY"
+        local symbol = " !!!!!!!!"
+
+        if GLOBAL.TheWorld.state.phase == "night" then
+            TIPS(title, c_night .. symbol)
+        elseif GLOBAL.TheWorld.state.phase == "dusk" then
+            TIPS(title, c_dusk .. symbol)
+        elseif GLOBAL.TheWorld.state.phase == "day" then
+            TIPS(title, c_day .. symbol)
+        end
+    end
+
     if GLOBAL.TheWorld.state.phase == "night" and AutoEnableAtNight then
         setNightVisionEnabled(inst, true)
     elseif GLOBAL.TheWorld.state.phase == "dusk" and AutoEnableAtDusk then
@@ -25,14 +49,26 @@ local function autoEnable(inst)
 end
 
 local function toggle()
+    -- 聊天不生效功能
+    if not (GLOBAL.TheFrontEnd:GetActiveScreen().name:find("HUD") ~= nil and not (GLOBAL.ThePlayer.HUD:IsControllerCraftingOpen() or GLOBAL.ThePlayer.HUD:IsControllerInventoryOpen())) then
+        return
+    end
     if GLOBAL.ThePlayer.local_nightvision == nil then
         GLOBAL.ThePlayer.local_nightvision = false
     end
-
     setNightVisionEnabled(GLOBAL.ThePlayer, not GLOBAL.ThePlayer.local_nightvision)
+end
+
+-- 自定义键位
+local ToggleEnableKey = GetModConfigData("ToggleHotkey") or "N"
+if type(ToggleEnableKey) == "string" then
+    ToggleEnableKey = ToggleEnableKey:lower():byte()
 end
 
 AddPlayerPostInit(function(inst)
     inst:WatchWorldState("phase", autoEnable)
+    if EnterGameAutoOpen then
+        setNightVisionEnabled(inst, true)
+    end
     GLOBAL.TheInput:AddKeyDownHandler(ToggleEnableKey, toggle)
 end)
