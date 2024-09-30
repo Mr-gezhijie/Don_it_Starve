7fc2743a1ab57e8c7b5dfaab502bfa787ccd6233
mr-gezhijie <1468531503@qq.com>
2024-10-01 03:07:15 +0800

    修改详情

--- src/main/AutoSwitchTools/modinfo.lua
+++ src/main/AutoSwitchTools/modinfo.lua
@@ -1,11 +1,35 @@
-name = "AAAAAAAAAAAAAAAAAA"
-author = "萌萌的新"
 
-description =
-[[包含以下要素，
-1.点击一个可工作的物品自动切装备（斧子稿子铲子锤子捕虫网镰刀）
-2.对于部分物品有右键功能（比如右键远古大门插钥匙）
-3.对于不同的角色右键可以做他们的动作（小恶魔和女武神和威洛）
+local CHS = locale == "zh" or locale == "zhr"
+--name = "AAAAAAAAAAAAAAAAAA"
+name = "AutoSwitchTools 自动装备工具"
+author = "GEZHIJIE"
+
+description = CHS and [[
+自动装备所需的工具
+
+功能：
+1。点击一个可工作的物品自动切装备（斧子稿子铲子锤子捕虫网镰刀）
+2。按住空格可砍树和挖矿
+3。对于部分物品有右键功能（比如右键远古大门插钥匙）
+4。对于不同的角色右键可以做他们的动作（小恶魔和女武神和威洛）
+
+对原版的优化：
+1。我之多加了一个，按住空格可砍树和挖矿
+
+此mod为搬运优化
+]] or [[
+Tools required for automatic equipment
+
+Function:
+1. Click on a working item to automatically cut equipment (axe, draft, shovel, hammer, insect catching net, sickle)
+2. Press and hold the space to cut down trees and mine
+3. For some items, there is a right-click function (such as right clicking on the ancient gate to insert the key)
+4. Right click on different characters to perform their actions (Little Devil, Valkyrie, and Willow)
+
+Optimization of the original version:
+1. I added one more, hold down the space to chop down trees and mine
+
+This mod is for handling optimization
 ]]
 forumthread = ""
 api_version = 10
@@ -18,10 +42,10 @@ dont_starve_compatible = false
 reign_of_giants_compatible = false
 shipwrecked_compatible = false
 
-icon_atlas = "icn.xml"
-icon = "icn.tex"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 
-version = "1.1.39"
+version = "1.1.1"
 server_filter_tags = {}
 local null_options = {
 	{ description = "", data = "" }
