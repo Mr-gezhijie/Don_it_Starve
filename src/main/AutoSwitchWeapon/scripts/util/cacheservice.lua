822a24f3564ba8ac2980a7e9f451f7d7ac46086b
mr-gezhijie <1468531503@qq.com>
2024-10-09 15:04:13 +0800

    自动武器，报错问题

--- src/main/AutoSwitchWeapon/scripts/util/cacheservice.lua
+++ src/main/AutoSwitchWeapon/scripts/util/cacheservice.lua
@@ -51,20 +51,7 @@ function CacheService:GetCachedItem(item)
     end
 
     if not Cache[item.prefab] then
-        local sim = TheWorld.ismastersim
-        TheWorld.ismastersim = true
-
-        local status, err = pcall(SpawnPrefab, item.prefab)
-        if status then
-            local clone = SpawnPrefab(item.prefab)
-            Cache[item.prefab] = CacheItem(clone)
-            clone:Remove()
-            TheWorld.ismastersim = sim
-        else
             Cache[item.prefab] = CacheItem(item)
-            TheWorld.ismastersim = sim
-        end
-
     end
 
     return Cache[item.prefab]
