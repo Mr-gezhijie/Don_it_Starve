6aff467c68a4228b4727c187b1ad01d1b95f8f53
mr-gezhijie <1468531503@qq.com>
2024-11-21 17:25:36 +0800

    自动照明，火女武器问题

--- src/main/AutoLight/modinfo.lua
+++ src/main/AutoLight/modinfo.lua
@@ -69,7 +69,7 @@ Welcome to leave a message for discussion!
 ]]
 
 author = "GEZHIJIE"
-version = "2.0.1"
+version = "2.0.2"
 forumthread = ""
 api_version_dst = 10
 icon_atlas = "modicon.xml"  -- logo地址
--- src/main/AutoLight/modmain.lua
+++ src/main/AutoLight/modmain.lua
@@ -5,7 +5,11 @@
 
 local GLOBAL = GLOBAL;
 local pcall = GLOBAL.pcall
+
+local RPC = GLOBAL.RPC
+local ACTIONS = GLOBAL.ACTIONS
 local EQUIPSLOTS = GLOBAL.EQUIPSLOTS
+local SendRPCToServer = GLOBAL.SendRPCToServer
 local TheInput = GLOBAL.TheInput
 local AllRecipes = GLOBAL.AllRecipes
 local GetTime = GLOBAL.GetTime
@@ -112,7 +116,8 @@ local function DoUnequip(inst)
     if (inst and inst.replica and inst.replica.inventory) then
         hasEquipped = false;
         local handEquipment =  GetEquippedItem(inst,EQUIPSLOTS.HANDS)
-        if handEquipment and IsCheckIfLighting(handEquipment) then
+        --// 判断手中的东西，如果是火女的武器，就不卸下
+        if handEquipment and IsCheckIfLighting(handEquipment) and handEquipment.prefab ~= "lighter" then
             inst.replica.inventory:UseItemFromInvTile(handEquipment)
         end
     end
